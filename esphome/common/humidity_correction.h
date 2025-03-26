cb43e2a5b15441e6d554d27efafcebf6d60b6c2a
Aaron Solochek <aarons@gmail.com>
2025-03-25 22:59:43 -0700

    * use throttle_average filter for sensors

--- esphome/common/humidity_correction.h
+++ esphome/common/humidity_correction.h
@@ -1,41 +1,43 @@
+#pragma once
+
 #include <math.h>
 
-float f_to_c(float tempF)
+inline float f_to_c(float tempF)
 {
     return ((tempF - 32) / 1.8);
 }
 
-float c_to_f(float tempC)
+inline float c_to_f(float tempC)
 {
     return (tempC * 1.8 + 32);
 }
 
-float c_to_k(float tempC)
+inline float c_to_k(float tempC)
 {
     return tempC + 273.15;
 }
 
-float f_to_k(float tempF)
+inline float f_to_k(float tempF)
 {
     return f_to_c(tempF) + 273.15;
 }
 
-float c_to_r(float tempC)
+inline float c_to_r(float tempC)
 {
     return (c_to_k(tempC) * 1.8);
 }
 
-float psia_to_mPa(float psia)
+inline float psia_to_mPa(float psia)
 {
     return psia * 68.9476;
 }
 
-float psia_to_pa(float psia)
+inline float psia_to_pa(float psia)
 {
     return psia * 6894.76;
 }
 
-float compute_dew_point(float tempC, float humidity)
+inline float compute_dew_point(float tempC, float humidity)
 {
     // const float a = 17.625;
     // const float b = 243.04;
@@ -48,16 +50,16 @@ float compute_dew_point(float tempC, float humidity)
     return (b * p) / (a - p);
 }
 
-float dew_point_to_RH(float tempC, float dpC)
+inline float dew_point_to_RH(float tempC, float dpC)
 {
   const float a = 17.625;
   const float b = 243.04;
 
-  float rh = 100 * (exp(a * dpC / (b + dpC)) / exp(a * tempC / (b + tempC)));
+  float rh = 100.0 * (exp(a * dpC / (b + dpC)) / exp(a * tempC / (b + tempC)));
   return rh;
 }
 
-float sat_pressure(float tempC)
+inline float sat_pressure(float tempC)
 {
     /// Using equation2 from https://www.engineersedge.com/calculators/water_vapor_saturation_pressure_15730.htm
     auto A = -1.0440397e4;
@@ -73,12 +75,12 @@ float sat_pressure(float tempC)
     return psia_to_pa(sat_pressure_psia);
 }
 
-float correct_humidity(float temperature1, float humidity1, float temperature2)
+inline float correct_humidity(float temperature1, float humidity1, float temperature2)
 {
     float temp1_k = c_to_k(temperature1);
     float temp2_k = c_to_k(temperature2);
     float humidity2 = humidity1 * sat_pressure(temperature1) * temp2_k / (sat_pressure(temperature2) * temp1_k);
 
-    ESP_LOGD("custom", "correcting %.2f%% @ %.2fºC to %.2f%% @ %.2fºC", humidity1, temperature1, humidity2, temperature2);
+//    ESP_LOGD("custom", "correcting %.2f%% @ %.2fºC to %.2f%% @ %.2fºC", humidity1, temperature1, humidity2, temperature2);
     return humidity2;
 }
